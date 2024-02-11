4a2fbdda72dd7290607fab33ce44a260ddcc9b2d
kalumkolla <159565187+kalumkolla@users.noreply.github.com>
2024-02-11 19:22:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94755666349', 'me', true],
+  ['94755666349', 'KD_BRO, true], 
   [''] 
 ] //Number of owners
 
